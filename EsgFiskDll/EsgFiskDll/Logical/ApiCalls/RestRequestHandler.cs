8f05d8c1bcaadc33e760834a74a964e70d771880
KlodianMaloku <klodianmaloku@gmail.com>
2021-08-16 01:24:34 +0200

    Finished and tested all methods for RegistersInvoice only

--- EsgFiskDll/EsgFiskDll/Logical/ApiCalls/RestRequestHandler.cs
+++ EsgFiskDll/EsgFiskDll/Logical/ApiCalls/RestRequestHandler.cs
@@ -4,6 +4,7 @@ using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +12,27 @@ namespace EsgFiskDll.Logical.ApiCalls
 {
     class RestRequestHandler<ReqType,RespType>
     {
-        internal static IRestResponse<RespType> MakeResquest(IRestClient restClient , IRestRequest restRequest, ReqType Body, FiskConfigs Configs)
+        //internal static IRestResponse<RespType> MakeResquest(IRestClient restClient , IRestRequest restRequest, ReqType Body, FiskConfigs Configs)
+        internal static IRestResponse<RespType> MakeResquest(IRestClient restClient, IRestRequest restRequest, ReqType Body, FiskConfigs Configs)
         {
-            restRequest.AddHeader("Content-Type", "application/json");
-            restRequest.AddHeader("Accept", "*/*");
-            restRequest.AddHeader("Connection", "keep-alive");
-            restRequest.AddJsonBody(Body);
-            HttpBasicAuthenticator authenticator = new HttpBasicAuthenticator(Configs.ApiUsername, Configs.ApiPassword);
-            authenticator.Authenticate(restClient, restRequest);
-            return restClient.Post<RespType>(restRequest);
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                                     | SecurityProtocolType.Tls11
+                                     | SecurityProtocolType.Tls12;
+                restRequest.AddHeader("Content-Type", "application/json");
+                restRequest.AddHeader("Accept", "*/*");
+                restRequest.AddHeader("Connection", "keep-alive");
+                restRequest.AddJsonBody(Body);
+                HttpBasicAuthenticator authenticator = new HttpBasicAuthenticator(Configs.ApiUsername, Configs.ApiPassword);
+                authenticator.Authenticate(restClient, restRequest);
+                return restClient.Post<RespType>(restRequest);
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Gatim ne dergim te kerkeses: " + ex.Message.ToString());
+            }
             
         }
     }
